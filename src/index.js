b30bd8486df54cb6c340b26ca94a0eac41c71ae6
Manish <manisksing@gmail.com>
2022-08-01 21:39:57 +0530

    2-key props and spread operator

--- src/index.js
+++ src/index.js
@@ -7,50 +7,50 @@ import './index.css';
 // const author = '';
 // const img = '';
 
-const firstBook = {
-  img: 'https://m.media-amazon.com/images/I/81StezluKUS._AC_UY327_FMwebp_QL65_.jpg',
-  title: 'The Complete Novels of Sherlock Holmes',
-  author: 'Arthur Conan Doyle '
-}
-
-const secondBook = {
-  img: 'https://m.media-amazon.com/images/I/917RlnXGwIL._AC_UY327_FMwebp_QL65_.jpg',
-  title: 'The Mighty Thor Vol. 1: Thunder In Her Veins (The Mighty Thor (2015-2018))',
-  author: 'Jason Aaron and Russell Dauterman '
-}
-
-const thirdBook = {
-  img: 'https://m.media-amazon.com/images/I/81SUEMfpwML._AC_UY327_FMwebp_QL65_.jpg',
-  title: 'Vote Loki (Vote Loki (2016))',
-  author: 'Christopher Hastings , Ralph Macchio, et al. '
-}
+const books = [
+  {
+    id:1,
+    img: 'https://m.media-amazon.com/images/I/81StezluKUS._AC_UY327_FMwebp_QL65_.jpg',
+    title: 'The Complete Novels of Sherlock Holmes',
+    author: 'Arthur Conan Doyle '
+  },
+  {
+    id:2,
+    img: 'https://m.media-amazon.com/images/I/917RlnXGwIL._AC_UY327_FMwebp_QL65_.jpg',
+    title: 'The Mighty Thor Vol. 1: Thunder In Her Veins (The Mighty Thor (2015-2018))',
+    author: 'Jason Aaron and Russell Dauterman '
+  },
+  {
+    id:3,
+    img: 'https://m.media-amazon.com/images/I/81SUEMfpwML._AC_UY327_FMwebp_QL65_.jpg',
+    title: 'Vote Loki (Vote Loki (2016))',
+    author: 'Christopher Hastings , Ralph Macchio, et al. '
+  },
+];
 
 function BookList() {
   return (
     <section className='booklist'>
-      <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}>
-        <p>
-          children Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae vel ullam, provident,
-           laboriosam accusantium odit corporis corrupti soluta dignissimos quasi dicta fugit, ducimus ratione! 
-           Suscipit porro quis voluptatum nobis reprehenderit.
-        </p>
-      </Book>
-      <Book img={secondBook.img} title={secondBook.title} author={secondBook.author} />
-      <Book img={thirdBook.img} title={thirdBook.title} author={thirdBook.author} />
+      {books.map((book)=>{
+        const {img , title, author } = book;
+        return (
+         //<Book img={img} title={title} author={author}/>
+         <Book key={book.id} book={book} />
+        )
+      })}
     </section>
   );
 }
 
 const Book = (props) => {
   //props = {img, title, author}
-  const { img, title, author ,children } = props;   
+  const { img, title, author} = props.book;   
   //console.log(props);
   return (
     <article className='book'>
       <img src={img} alt='Books' />
       <h1>{title}</h1> 
       <h4>by {author} </h4>
-      {children}
     </article>
   );
 }
