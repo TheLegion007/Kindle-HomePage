6407c1c0229292a89a39642ba372cc4e12bb32db
Manish <manisksing@gmail.com>
2022-08-15 01:03:01 +0530

    3-Events Basics

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ function BookList() {
         const {img , title, author } = book;
         return (
          //<Book img={img} title={title} author={author}/>
-         <Book key={book.id} book={book} />
+         <Book key={book.id} {...book} />
         )
       })}
     </section>
@@ -44,13 +44,25 @@ function BookList() {
 
 const Book = (props) => {
   //props = {img, title, author}
-  const { img, title, author} = props.book;   
+  const { img, title, author} = props;   
   //console.log(props);
+  //attribute , eventHandler
+  //onClick, onMouseOver
+  const clickHandler = () => {
+    console.log('function called');   // called as soon as render //
+  }
+
+  const complexClick = (author) => {
+    console.log(author);
+  }
+
   return (
     <article className='book'>
       <img src={img} alt='Books' />
       <h1>{title}</h1> 
       <h4>by {author} </h4>
+      <button onClick={clickHandler}>Click Me</button>
+      <button onClick={() => complexClick(author)}>Second Func</button>
     </article>
   );
 }
